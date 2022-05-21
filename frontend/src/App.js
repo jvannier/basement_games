9abb24c0044627fd2c3472ad91d49b960ce9864f
Jennifer Vannier <Jen@Jennifers-MacBook-Pro.local>
2022-05-21 16:44:53 -0400

    wip

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+// var parse = require('pg-connection-string').parse;
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
+
+// client.connect();
+
+// const { Sequelize } = require('sequelize');
+// const { Pool, Client } = require('pg')
+
+// const pool = new Pool("postgres://gbiqgljmdxjizc:676b4d8d378de68419d49d17f068b5041717d6e0d24a7b1427f2951a472473ab@ec2-3-231-82-226.compute-1.amazonaws.com:5432/dds62fgfg23dch")
+
+// const sequelize = new Sequelize("postgres://gbiqgljmdxjizc:676b4d8d378de68419d49d17f068b5041717d6e0d24a7b1427f2951a472473ab@ec2-3-231-82-226.compute-1.amazonaws.com:5432/dds62fgfg23dch")
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Test
+      </header>
+    </div>
+  );
+}
+
+export default App;
