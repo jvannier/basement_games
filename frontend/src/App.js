f82529b73951f3c7e302aa28aee28ea5dbc5dde9
Jennifer Vannier <Jen@Jennifers-MacBook-Pro.local>
2022-05-21 17:21:41 -0400

    First try at API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,22 @@
+import React, { useEffect, useState } from "react";
 import './App.css';
-// var parse = require('pg-connection-string').parse;
-// const { Client } = require('pg');
 
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-// client.connect();
-
-// const { Sequelize } = require('sequelize');
-// const { Pool, Client } = require('pg')
-
-// const pool = new Pool("postgres://gbiqgljmdxjizc:676b4d8d378de68419d49d17f068b5041717d6e0d24a7b1427f2951a472473ab@ec2-3-231-82-226.compute-1.amazonaws.com:5432/dds62fgfg23dch")
-
-// const sequelize = new Sequelize("postgres://gbiqgljmdxjizc:676b4d8d378de68419d49d17f068b5041717d6e0d24a7b1427f2951a472473ab@ec2-3-231-82-226.compute-1.amazonaws.com:5432/dds62fgfg23dch")
 
 function App() {
+  const [results, setResults] = useState("NO RESULTS")
+  useEffect(async () => {
+    const response = await fetch(
+      `https://basement-games.herokuapp.com/`
+    );
+    const data = await response.json()
+    setResults(data);
+  }, [results]);
+
   return (
     <div className="App">
       <header className="App-header">
         Test
+        { results }
       </header>
     </div>
   );
--- server.js
+++ server.js
@@ -1,4 +1,32 @@
+const { Client } = require('pg');
 const express = require("express");
 const app = express()
-const { Sequelize } = require('sequelize')
+app.use(express.json())
+app.use(express.static('build'));
+app.use(express.urlencoded({ extended: false }))
 
+const port = process.env.PORT || 3001;
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+
+// ROOT
+app.get('/', async (req, res) => {
+    let err, query_result = await client.query('SELECT * FROM testing;')
+    console.log(err, query_result)
+    let response = []
+    for (let row of query_result.rows) {
+        response.push(JSON.stringify(row))
+    }
+    res.status(200).json({
+        message: JSON.stringify(response)
+    })
+})
+app.listen(port, () => {});
